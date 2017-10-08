73bca59fcde308907bb995595e07ec4324273261
L33nny <deathboy@op.pl>
2017-10-08 22:44:43 +0200

    add

---
+++ Assets/Skrypt/changeIconFuel.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class changeIconFuel : MonoBehaviour {
+
+    public Image ToChange;
+    public Sprite Full,Empty;
+    int zmiana;
+
+	void Start ()
+    {
+	}
+
+    void Update()
+    {
+        zmiana = FindObjectOfType<carController>().ileZyc;
+        if(zmiana == 1)
+        {
+            ToChange.sprite = Full;
+        }
+        else if (zmiana == 0)
+        {
+            ToChange.sprite = Empty;
+        }
+    }
+}
---
+++ Assets/Skrypt/destroy.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+ 
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(coll.gameObject);
+    }
+    
+}
